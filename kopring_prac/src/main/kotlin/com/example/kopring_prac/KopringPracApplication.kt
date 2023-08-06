b9a4d40d5c053d56e201c3e31f55650fe4ba70ca
CH_Hwang <hch950627@naver.com>
2023-08-06 16:15:22 +0900

    initial commit

---
+++ kopring_prac/src/main/kotlin/com/example/kopring_prac/KopringPracApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopring_prac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringPracApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringPracApplication>(*args)
+}
---
+++ kopring_prac/src/test/kotlin/com/example/kopring_prac/KopringPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kopring_prac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringPracApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
