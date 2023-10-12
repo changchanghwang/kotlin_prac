9ce84f91e645b72d2df4ab2ff8cb52e77b95f576
changhwan hwang <ch.hwang@ecubelabs.com>
2023-10-13 08:34:55 +0900

    fix:minor

--- todot_list/src/main/kotlin/com/example/todot_list/todo/domain/Todo.kt
+++ todot_list/src/main/kotlin/com/example/todot_list/todo/domain/Todo.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import java.util.*
 
 @Entity
 @Table(name="todo")
@@ -15,5 +16,5 @@ class Todo(
     var title: String,
 
     @Column(nullable = false, length = 10, name="writerId")
-    var writerId: String,
+    var writerId: UUID,
 )
